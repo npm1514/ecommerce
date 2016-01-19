d79dde39a7e8e3a1b1a459cfa64d379c976093fe
Nick Marucci <npm1514@gmail.com>
2016-01-19 14:25:42 -0700

    connected front to back

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -17,7 +17,11 @@ app.config(function($urlRouterProvider, $stateProvider) {
 app.controller('mainCtrl',function ($scope, mainService) {
   $scope.products = {};
   $scope.getProducts = function () {
-    $scope.products = mainService.getProducts();
+    mainService.getProducts().then(function(response){
+      console.log('test2');
+      console.log(response);
+      $scope.products = response;
+    });
   };
   $scope.getProducts();
 
@@ -25,10 +29,12 @@ app.controller('mainCtrl',function ($scope, mainService) {
 
 app.service('mainService', function($http){
   this.getProducts = function () {
+    console.log('test1');
     return $http ({
       method: "GET",
       url: '/products',
     }).then(function (response) {
+
       return response.data;
     });
   };
